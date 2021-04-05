a2249c3b85070653f841bf297eb62108faf992f8
John Steel <git@jskw.dev>
2021-04-04 22:58:28 -0400

    Refactor router
    + Change get to post
    + Add more arguments
    + Removed unessisary stuff

--- src/main/kotlin/dev/jskw/tls/plugins/Routing.kt
+++ src/main/kotlin/dev/jskw/tls/plugins/Routing.kt
@@ -22,73 +22,92 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-        get<Scan> { scanArguments ->
-            val file = File("cache/${scanArguments.name}.html")
-            if(false) {
-                call.respondText("Location: name=${scanArguments.name}, arg1=${scanArguments.arg1}, arg2=${scanArguments.arg2}\n")
-            } else {
-                var fileOutputStream: FileOutputStream? = null
-                var fileLock : FileLock? = null
-                val getLock = launch(Dispatchers.IO) { // will get dispatched to DefaultDispatcher
-                    fileOutputStream = FileOutputStream(file);
+        post<Scan> { scanArguments ->
+            val file = File("cache/${scanArguments.host}.html")
+            var fileOutputStream: FileOutputStream? = null
+            var fileLock : FileLock? = null
 
-                    try {
-                        fileLock = try {
-                            fileOutputStream!!.channel.tryLock()
-                        } catch (exception: Exception) {
-                            throw exception;
-                        }
-                    call.application.environment.log.trace("Lock aquired for ${file.absolutePath}.")
+            val getLock = launch(Dispatchers.IO) { // will get dispatched to DefaultDispatcher
+                fileOutputStream = FileOutputStream(file);
+
+                try {
+                    fileLock = try {
+                        fileOutputStream!!.channel.tryLock()
                     } catch (exception: Exception) {
-                        call.application.environment.log.trace("Failed to get lock: ", exception);
-                        return@launch
+                        throw exception;
                     }
+                call.application.environment.log.trace("Lock aquired for ${file.absolutePath}.")
+                } catch (exception: Exception) {
+                    call.application.environment.log.trace("Failed to get lock: ", exception);
+                    return@launch
                 }
+            }
 
-                getLock.join()
-                if (fileLock?.isValid != true) {
-                    call.respond(HttpStatusCode(420, "Enhance your calm"));
-                    return@get
-                }
+            getLock.join()
+            if (fileLock?.isValid != true) {
+                call.respond(HttpStatusCode(420, "Enhance your calm"));
+                return@post
+            }
 
-                call.respond(HttpStatusCode.Accepted);
-                launch(Dispatchers.IO) {
-                    call.application.environment.log.debug("${Thread.currentThread().name}: Before use lock: ${fileLock?.isValid}")
-                    fileOutputStream.use { stream ->
-                        call.application.environment.log.debug("${Thread.currentThread().name}: Start scan")
-                        call.application.environment.log.debug("${Thread.currentThread().name}: Use lock: ${fileLock?.isValid}")
-                        val process = scanArguments.run(file.parentFile, stream!!)
-                        process[1].outputStream.use {
-                            call.application.environment.log.debug("${Thread.currentThread().name}: Waiting 0")
-                            process[0].inputStream.transferTo(it);
-                            call.application.environment.log.debug("${Thread.currentThread().name}: Finished")
-                        }
-                        launch(Dispatchers.IO) {
-                            call.application.environment.log.debug("${Thread.currentThread().name}: Waiting 1")
-                            process[1].inputStream.transferTo(stream);
-                            call.application.environment.log.debug("${Thread.currentThread().name}: Finished")
-                        }
-                        call.application.environment.log.debug("${Thread.currentThread().name}: Waiting Scan")
-                        process[0].waitFor()
-                        call.application.environment.log.debug("${Thread.currentThread().name}: Finished scan")
-                        process[1].waitFor()
-                        call.application.environment.log.debug("${Thread.currentThread().name}: Finished format")
+            call.respond(HttpStatusCode.Accepted);
+            launch(Dispatchers.IO) {
+                fileOutputStream.use { stream ->
+                    call.application.environment.log.debug("${Thread.currentThread().name}: Start scan")
+
+                    val process = scanArguments.run()
+                    process[1].outputStream.use {
+                        call.application.environment.log.debug("${Thread.currentThread().name}: Waiting 0")
+                        process[0].inputStream.transferTo(it);
+                        call.application.environment.log.debug("${Thread.currentThread().name}: Finished")
                     }
+                    launch(Dispatchers.IO) {
+                        call.application.environment.log.debug("${Thread.currentThread().name}: Waiting 1")
+                        process[1].inputStream.transferTo(stream);
+                        call.application.environment.log.debug("${Thread.currentThread().name}: Finished")
+                    }
+                    call.application.environment.log.debug("${Thread.currentThread().name}: Waiting Scan")
+                    process[0].waitFor()
+                    call.application.environment.log.debug("${Thread.currentThread().name}: Finished scan")
+                    process[1].waitFor()
+                    call.application.environment.log.debug("${Thread.currentThread().name}: Finished format")
                 }
             }
         }
     }
 }
 
-@Location("/scan/{name}")
-class Scan(val name: String, val arg1: Int = 42, val arg2: String = "0")
 
-fun Scan.run(workingDir: File, fileOutputStream: FileOutputStream): Array<Process> {
-    val scanCommand = arrayOf("../sslscan", this.name)
-    val formatCommand = arrayOf("aha", "--black")
+/**
+ * Scan arguments for sslscan
+ *
+ * @property host the host to scan
+ * @property port the port to scan
+ * @property heartbleed check for OpenSSL Heartbleed (CVE-2014-0160)
+ * @property renegotiation check for TLS renegotiation
+ * @constructor Creates an empty group.
+ */
+@Location("/scan/{host}")
+class Scan(val host: String, val port: Int = 443, val heartbleed: Boolean = true, val renegotiation: Boolean = true)
 
-    val scanProcessBuilder = ProcessBuilder(*scanCommand).directory(workingDir)
-    val formatProcessBuilder = ProcessBuilder(*formatCommand).directory(workingDir)
+
+fun Scan.run(): Array<Process> {
+    val scanCommand = mutableListOf("./sslscan")
+
+    if (!heartbleed) {
+        scanCommand.add("--no-heartbleed")
+    }
+
+    if (!renegotiation) {
+        scanCommand.add("--no-renegotiation")
+    }
+
+    scanCommand.add("$host:$port")
+
+    val formatCommand = arrayOf("aha", "--black", "--title", scanCommand.toString())
+
+    println(scanCommand)
+    val scanProcessBuilder = ProcessBuilder(*scanCommand.toTypedArray())
+    val formatProcessBuilder = ProcessBuilder(*formatCommand)
 
     return arrayOf(scanProcessBuilder.start(), formatProcessBuilder.start())
 }
